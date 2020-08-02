b95125379730a5d6d339dbeb6c1ad8eb645cab20
kamrulbari14 <kamrulbpriyo0523@gmail.com>
2020-08-02 15:13:34 +0600

    correct the spelling

--- src/com/luv2code/springdemo/DemoClass.java
+++ src/com/luv2code/springdemo/DemoClass.java
@@ -2,6 +2,6 @@ package com.luv2code.springdemo;
 
 public class DemoClass {
 	public static void main(String[] args) {
-		System.out.println("Doiing Great");
+		System.out.println("Doing Great");
 	}
 }
